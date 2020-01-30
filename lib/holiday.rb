666ea0fa90517841469f4b77a582108dbc687069
James Rogers <jarogers095@gmail.com>
2020-01-30 04:32:47 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -61,7 +61,7 @@ def all_supplies_in_holidays(holiday_hash)
   holiday_hash.each do |season, holidays|
     puts "#{season.to_s.capitalize}:"
     holidays.each do |holiday, supplies|
-      print "#{holiday.to_s.capitalize}: #{holiday_hash[season][holiday]}"
+      print "#{holiday.to_s.capitalize}: #{holiday_hash[season][holiday].flatten}"
     end
   end
 end
